e89aabc32e907d15713da43ceabc384f681097d6
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-08-28 14:49:40 -0400

    state for input and output

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
 
   return (
     <div className="App">
-      <input type="text" />
-      <div className="terminal"></div>
+      <input 
+        type="text" 
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
+      <div className="terminal">
+        {output}
+      </div>
     </div>
   )
 }
