342f8a5017f020b3ecac9fbd66dcb7c90949cef7
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-08-28 14:40:26 -0400

    add input and div for terminal

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className="App">
+      <input type="text" />
+      <div className="terminal"></div>
+    </div>
   )
 }
 
